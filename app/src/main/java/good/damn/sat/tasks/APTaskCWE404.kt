a2a87f7dd48efec120638dcf9c3418b3632d7ee6
GoodDamn <grigorydum@gmail.com>
2026-03-11 08:31:46 +0300

    app:: APTaskCWE404: refactor

--- app/src/main/java/good/damn/sat/tasks/APTaskCWE404.kt
+++ app/src/main/java/good/damn/sat/tasks/APTaskCWE404.kt
@@ -8,52 +8,51 @@ import java.io.FileOutputStream
 class APTaskCWE404
 : APITask {
 
-    companion object {
-        private const val TAG = "APTaskCWE404"
-    }
-
-    private val mFile = File(
-        Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_DOCUMENTS
-        ),
-        "3.mp3"
-    ).apply {
-        if (!exists() && createNewFile()) {
-            Log.d(TAG, "execute: $name is created")
-        }
-    }
-
-    private val mFile2 = File(
-        Environment.getExternalStoragePublicDirectory(
-            Environment.DIRECTORY_DOCUMENTS
-        ),
-        "33.mp3"
-    ).apply {
-        if (!exists() && createNewFile()) {
-            Log.d(TAG, "execute: $name is created")
-        }
-    }
-
-    private val outStreams = Array(
-        Short.MAX_VALUE.toInt() / 2
-    ) {
-        FileOutputStream(
-            mFile
+    private val outStreams = createStreams(
+        createFile(
+            "3.mp3"
         )
-    }
+    )
 
-    private val outStreams2 = Array(
-        Short.MAX_VALUE.toInt() / 2
-    ) {
-        FileOutputStream(
-            mFile2
+    private val outStreams2 = createStreams(
+        createFile(
+            "33.mp3"
         )
-    }
+    )
 
     override fun execute() {
         outStreams.forEach { _ -> }
         outStreams2.forEach { _ -> }
+        Log.d(
+        "TAG",
+        "execute: descriptors size:" +
+                "${outStreams.size} + ${outStreams2.size}" +
+                "= ${outStreams.size + outStreams2.size}"
+        )
     }
 
     override fun stop() = Unit
+
+    private inline fun createFile(
+        child: String
+    ) = File(
+        Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_DOCUMENTS
+        ),
+        child
+    ).apply {
+        if (!exists() && createNewFile()) {  }
+    }
+
+    private inline fun createStreams(
+        file: File
+    ) = Array(
+        Short.MAX_VALUE.toInt() / 2
+    ) {
+        FileOutputStream(
+            file
+        ).apply {
+            close()
+        }
+    }
 }
